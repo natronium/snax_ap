b64a4727a4e9f21408bf344fc41426f49ac3ef26
Na <>
2025-08-18 13:19:06 -0400

    Proof of concept hooking add_to_inventory function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::process::Command;
+use std::{io::{BufRead, BufReader}, process::{Command, Stdio}};
 use constcat::concat;
+use dll_syringe::{process::OwnedProcess, Syringe};
 
 const BUGSNAX_DIR: &str = r#"C:\Program Files (x86)\Steam\steamapps\common\Bugsnax"#;
 const BUGSNAX_EXE_PATH: &str = concat!(BUGSNAX_DIR, "\\Bugsnax.exe");
@@ -7,8 +8,24 @@ const BUGSNAX_EXE_PATH: &str = concat!(BUGSNAX_DIR, "\\Bugsnax.exe");
 fn main() {
     let mut child = Command::new(BUGSNAX_EXE_PATH)
         .current_dir(BUGSNAX_DIR)
+        .stdout(Stdio::piped())
         .spawn()
         .expect("Failed to launch bugsnax");
-    let ecode = child.wait();
-    println!("Bugsnax exited with code: {ecode:#?}");
+
+    let target_process = OwnedProcess::from_pid(child.id()).expect("could not find child process by id");
+    let syringe = Syringe::for_process(target_process);
+    let _injected_payload = syringe.inject("./target/debug/snax_lib.dll").unwrap();
+    
+    let snax_stdout = child.stdout.take().expect("could not take snax stdout");
+
+    let reader = BufReader::new(snax_stdout);
+
+    for line in reader.lines() {
+        let line = line.expect("could not read line from snax stdout");
+        println!("{line}");
+    }
+
+
+    let ecode = child.wait().unwrap();
+    println!("Bugsnax exited with code: {ecode}");
 }
